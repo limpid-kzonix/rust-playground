80a873404bbfa6683169e7cfda301bb499598542
limpid-kzonix <balyszyn@gmail.com>
2024-07-28 19:38:00 +0000

    add https://www.codewars.com/kata/54da5a58ea159efa38000836

---
+++ katastrophes/src/find_odd.rs
@@ -0,0 +1,36 @@
+// Kata: Find the odd int
+// Link: https://www.codewars.com/kata/54da5a58ea159efa38000836
+// Author: https://www.codewars.com/users/rbuckley   
+
+use std::collections::HashMap;
+
+fn find_odd(arr: &[i32]) -> i32 {
+    
+    let mut counts: HashMap<i32, i32>= HashMap::new();
+
+    for i in arr.iter() {
+
+        let updated = counts.get(i).unwrap_or(&0);
+        counts.insert(*i, *updated + 1);
+
+        
+    }
+
+
+    let mut res: i32 = 0; 
+    for (k, v) in counts.iter()  {
+        if v % 2 != 0 {
+            res = *k;
+            break;
+        } else {
+            res = *k;
+        }
+    }
+
+    return res;
+}
+
+pub fn run() {
+    let res = find_odd(&[1,1,2]);
+    println!("{}", res);
+}
--- katastrophes/src/main.rs
+++ katastrophes/src/main.rs
@@ -1,6 +1,8 @@
 mod create_phone_numbers;
+mod find_odd;
 
 fn main() {
     println!("Hello, world!");
     create_phone_numbers::run();
+    find_odd::run();
 }

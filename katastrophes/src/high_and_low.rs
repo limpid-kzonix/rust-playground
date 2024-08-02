07fccf543af2808a105cc05341518ecc96dec8ff
limpid-kzonix <balyszyn@gmail.com>
2024-08-02 21:15:48 +0000

    Add https://www.codewars.com/kata/554b4ac871d6813a03000035

---
+++ katastrophes/src/high_and_low.rs
@@ -0,0 +1,8 @@
+// Kata: Highest and Lowest
+// Link: https://www.codewars.com/kata/554b4ac871d6813a03000035/train/rust
+// Author: https://www.codewars.com/users/Deantwo
+
+
+fn high_and_low(numbers: &str) -> String {
+    String::new()
+}
